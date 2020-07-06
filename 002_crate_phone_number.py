23e1b93af5abbb8234f61a6a1d1a28ac3ed88e4e
Mehmet <fetihmurat27@gmail.com>
2020-07-06 18:23:17 +0200

    first commit

---
+++ 002_crate_phone_number.py
@@ -0,0 +1,7 @@
+# Write a function that accepts an array of 10 integers (between 0 and 9), 
+# that returns a string of those numbers in the form of a phone number.
+
+def create_phone_number(n):
+    return '({}{}{}) {}{}{}-{}{}{}{}'.format(p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9])
+
+   
